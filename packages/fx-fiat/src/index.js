507ff96b717bac4d62b3571cfba6035f5cfe80e4
Josh G <129348785+Josh-Gi3r@users.noreply.github.com>
2026-08-09 00:44:16 +0800

    FX-6: export settlement graph and verifier adapters

--- packages/fx-fiat/src/index.js
+++ packages/fx-fiat/src/index.js
@@ -1,2 +1,4 @@
 export { EDGE_TYPES, FINALITY, hashFiatIntent, normalizeFiatIntent } from './intent.js';
 export { FiatSettlementStore, INTENT_STATES, hashRef } from './settlement-store.js';
+export { SettlementGraph, normalizeSettlementEdge, routeGuarantee } from './settlement-graph.js';
+export { InternalLedgerVerifier, AttestedPaymentVerifier } from './adapters.js';
