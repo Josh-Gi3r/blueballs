0c69b71081c30a54340cb6b8e45db733a7b25f95
Josh G <129348785+Josh-Gi3r@users.noreply.github.com>
2026-08-08 19:43:57 +0800

    FX-1: format Foundry cheatcode interface

--- packages/fx-contracts/test/utils/Vm.sol
+++ packages/fx-contracts/test/utils/Vm.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.8.36;
 
 interface Vm {
     function addr(uint256 privateKey) external returns (address);
-    function sign(uint256 privateKey, bytes32 digest) external returns (uint8 v, bytes32 r, bytes32 s);
+    function sign(uint256 privateKey, bytes32 digest)
+        external
+        returns (uint8 v, bytes32 r, bytes32 s);
     function prank(address msgSender) external;
     function startPrank(address msgSender) external;
     function stopPrank() external;
