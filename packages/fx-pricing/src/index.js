63211998c84aca3968ddc001262304987cbd213d
Josh G <129348785+Josh-Gi3r@users.noreply.github.com>
2026-08-08 23:33:48 +0800

    FX-4: export pricing primitives

---
+++ packages/fx-pricing/src/index.js
@@ -0,0 +1,10 @@
+export {
+  Rational,
+  ceilDiv,
+  floorDiv,
+  decimalToRational,
+  applyBps,
+  convertAtomic,
+} from './rational.js';
+export { ReferencePriceBook } from './reference.js';
+export { PrincipalRiskBook } from './risk-book.js';
